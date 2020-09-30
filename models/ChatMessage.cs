928b4e61900f1e4e7e5af334aa293abedba7fbd9
latte1635 <lauri.lundell@outlook.com>
2020-09-30 13:14:41 +0300

    namespaces

--- models/ChatMessage.cs
+++ models/ChatMessage.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-
-public class ChatMessage
+namespace NW.Models
 {
-    public Character Sender { get; set; }
-    public List<Character> Listeners { get; set; }
-    public AccessRole SenderRole { get; set; }
-    public string Message { get; set; }
-    public DateTime TimeStamp { get; set; }
-    public Vector3 Location { get; set; }
-    public MessageStyle Style { get; set; }
+    public class ChatMessage
+    {
+        public Character Sender { get; set; }
+        public List<Character> Listeners { get; set; }
+        public AccessRole SenderRole { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Vector3 Location { get; set; }
+        public MessageStyle Style { get; set; }
+    }
 }
--- models/Death.cs
+++ models/Death.cs
@@ -1,10 +1,13 @@
 using System;
 
-public class Death
+namespace NW.Models
 {
-    public Character Killer { get; set; }
-    public Character Killed { get; set; }
-    public string Weapon { get; set; }
-    public DateTime TimeStamp { get; set; }
-    public bool FriendOnFriend { get; set; }
+    public class Death
+    {
+        public Character Killer { get; set; }
+        public Character Killed { get; set; }
+        public string Weapon { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool FriendOnFriend { get; set; }
+    }
 }
