27672190b9533a6385f9d51350be93e3c01b762f
latte1635 <lauri.lundell@outlook.com>
2020-10-03 19:20:25 +0300

    discord working

--- src/Discord/Discord.cs
+++ src/Discord/Discord.cs
@@ -17,7 +17,7 @@ namespace NW.Discord
 
             //  You can assign your bot token to a string, and pass that in to connect.
             //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = File.ReadAllText("discord-key.txt");
+            string token = File.ReadAllText("discord-key.txt");
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
