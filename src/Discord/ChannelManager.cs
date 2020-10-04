e737eb7b22010cba6385c2620d18f1f9d65cad0c
Alex <alex.leppakoski@hotmail.com>
2020-10-04 13:59:55 +0200

    feat: discord channels

---
+++ src/Discord/ChannelManager.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NW.Discord
+{
+    public class ChannelManager
+    {
+        public ulong AnnouncementChannelID {get; set;}
+        public ulong FeedChannelID {get; set;}
+        public ulong QueryChannelID {get; set;}
+    }
+}
